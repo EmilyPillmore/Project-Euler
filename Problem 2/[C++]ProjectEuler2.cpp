f5221213de5b72a68460172e4e5bace763ab0474
Shawn Smith <ShawnSmith0828@gmail.com>
2011-07-23 22:36:09 -0400

    Did the second problem in C++

---
+++ Problem 2/[C++]ProjectEuler2.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	// Define the variables
+	int x = 1;
+	int y = 1;
+	int last = 1;
+	int sum = 0;
+
+	// Check every number under 4 milion
+	while (x <= 4000000)
+	{
+
+		// If the checking number is even, add it to the sum
+		if (x % 2 == 0) sum += x;
+
+		// Set last to x, x to x+y, y to last.
+		last = x;
+		x += y;
+		y = last;
+	}
+
+	// Print the answer
+	printf("Answer: %d\r\n", sum);
+}
