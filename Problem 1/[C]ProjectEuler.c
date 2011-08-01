b1dae206f643faadaff13830b7c892328dc8f0e2
Shawn Smith <ShawnSmith0828@gmail.com>
2011-08-01 07:02:33 -0400

    Updated problem 1

--- Problem 1/[C]ProjectEuler.c
+++ Problem 1/[C]ProjectEuler.c
@@ -8,14 +8,11 @@ int main()
 	int sum = 0;
 
 	/* While checking every number under 1000 */
-	while(counter < 1000)
+	for(counter = 1; counter < 1000; counter++)
 	{
 		/* If the checking number is devisible by 3 or 5
 		then add it to the sum */
-		if ((counter % 3 == 0) || (counter % 5 == 0)) sum = counter+sum;
-
-		/* Incrase the counter */
-		counter++;
+		if (counter % 3 == 0 || counter % 5 == 0) sum = counter+sum;
 	}
 
 	/* Print the answer */
