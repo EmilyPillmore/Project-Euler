046b233ab028811a1c89a7e0ca56e4a794b01915
Shawn Smith <ShawnSmith0828@gmail.com>
2011-09-27 04:02:31 -0400

    Updated Solution 12.c, now works in roughly 5 seconds.

--- Problem 012/Solution 12.c
+++ Problem 012/Solution 12.c
@@ -1,32 +1,38 @@
+/* Solves http://projecteuler.net/problem=12
+ * Requires -lm linker flags on some compilers.
+ * gcc Solution\ 12.c -lm */
+
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
 	/* vars */
 	int number = 0;
-	int counter = 1;
 	int check = 0;
+	int counter = 1;
 	int divisors = 0;
 
-	/* loop until we break */
-	while(1)
+	/* Loop while divisors is under 500 */
+	while(divisors <= 250)
 	{
 		printf("Checking: %d\r\n", number);
 		number += counter;
-		counter++;
-
 		check = 1;
 		divisors = 0;
 
 		/* Check every number under number/2 */
-		for(check = 1; check <= number/2; check++)
+//		for(check = 1; check <= number/2; check++)
+		for(check = 1; check <= sqrt(number); check++)
 		{
 			if(number % check == 0) divisors++;
 		}
 
-		/* If divisors > 500, got the answer, break */
-		if(divisors > 500) break;
+		counter++;
+
 	}
 
 	printf("Answer: %d\r\n", number);
+
+	return 0;
 }
