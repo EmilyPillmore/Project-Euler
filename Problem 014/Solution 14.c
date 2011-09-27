b1b9ac9e3c1becf3a1815bae45f6d2010a3c568e
Shawn Smith <ShawnSmith0828@gmail.com>
2011-09-27 04:43:19 -0400

    Added a solution to problem 14 in c

---
+++ Problem 014/Solution 14.c
@@ -0,0 +1,56 @@
+/* Solves http://projecteuler.net/problem=14 */
+
+#include <stdio.h>
+
+/* Do the even or odd stuff. */
+size_t Even(size_t number)
+{
+	return number/2;
+}
+
+size_t Odd(size_t number)
+{
+	return 3*number+1;
+}
+
+int main()
+{
+	/* Our variables. */
+	int l_number = 0;
+	size_t check = 0;
+	int l_counter = 0;
+	int counter = 0;
+	int i = 1;
+
+	/* We only check odd numbers because any even number will have a shorter
+		chain since it's just being divided by two all the way down to 1. */
+	for(i = 1; i <= 1000000; i+=2)
+	{
+		printf("Checking number: %d\r\n", i);
+		check = i;
+		counter = 0;
+		while (check != 1)
+		{
+			if (check % 2 == 0)
+				check = Even(check);
+			else
+				check = Odd(check);
+
+			counter++;
+
+		}
+
+		if (l_counter < counter)
+		{
+			l_number = i;
+			l_counter = counter;
+			printf("Largest chain update: %d | Number is: %d\r\n", l_counter, l_number);
+		}
+
+	}
+
+	printf("Number with the longest chain: %d\r\n", l_number);
+	printf("The chain was %d numbers long\r\n", l_counter);
+
+	return 0;
+}
