a95209d2b0175e8b0eed0116967200923547c2ab
Shawn Smith <ShawnSmith0828@gmail.com>
2011-09-28 00:06:29 -0400

    Added a ruby solution to problem 7

---
+++ Problem 007/Solution 7.rb
@@ -0,0 +1,38 @@
+# Solves http://projecteuler.net/problem=7
+
+def CheckPrime(number)
+
+	# We start at 3 because 1 and 2 are prime, and we don't
+	# check them
+	x = 3
+	limit = number
+
+	while x < limit
+		if (number % x == 0)
+			return 0
+		end
+
+		limit = number / x
+		x += 2
+	end
+
+	return 1
+end
+
+if __FILE__ == $0
+	x = 1
+	pc = 3
+
+	while(1)
+		if(CheckPrime(pc) == 1)
+			x += 1
+		end
+		if x == 10001
+			break
+		end
+
+		pc += 2
+	end
+
+	print "Answer: ", pc, "\n"
+end
