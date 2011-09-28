6af691385f766f7fa7f683b5b9be0ece0a1d1418
Shawn Smith <ShawnSmith0828@gmail.com>
2011-09-27 23:54:35 -0400

    Solution to problem 6 in ruby

---
+++ Problem 006/Solution 6.rb
@@ -0,0 +1,16 @@
+# Solves http://projecteuler.net/problem=6
+
+if __FILE__ == $0
+
+	sum = 0
+	sq_sum = 0
+
+	for n in (1..100)
+		sum += n*n
+		sq_sum += n
+	end
+
+	sq_sum *= sq_sum
+
+	print "Difference: ", sq_sum-sum, "\n"
+end
