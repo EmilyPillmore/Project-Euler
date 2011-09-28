35895f6870ae000c1fbfdc7aa68d910f0db62afa
Shawn Smith <ShawnSmith0828@gmail.com>
2011-09-27 23:47:47 -0400

    Solution to problem 5 in ruby, takes a bit but it does work

---
+++ Problem 005/Solution 5.rb
@@ -0,0 +1,29 @@
+# Solves http://projecteuler.net/problem=5
+
+if __FILE__ == $0
+
+	x = 0
+	check = 0
+	y = 0
+
+	while(1)
+		# We only check multiples of 10 because it has
+		# to end in 0 to be divisible by 10 or 20
+		x += 10
+
+		# Loop to check if it's divisible by 1-20
+		for y in (1..20)
+			if (x % y == 0)
+				check += 1
+			end
+		end
+
+		if check == 20
+			break
+		else
+			check = 0
+		end
+	end
+
+	print "Answer: ", x, "\n"
+end
