1eaab3b23160c4286c44bcfc228f3a2a49de1d77
Shawn Smith <ShawnSmith0828@gmail.com>
2011-09-28 01:25:37 -0400

    Added a solution to problem 9 in ruby, takes roughly 8-9 minutes to solve though

---
+++ Problem 009/Solution 9.rb
@@ -0,0 +1,19 @@
+# Solves http://projecteuler.net/problem=9
+
+if __FILE__ == $0
+
+	for a in (1..1000)
+		for b in (1..1000)
+			for c in (1..1000)
+				if(a+b+c == 1000)
+					if(a*a + b*b == c*c)
+						product = a*b*c
+					end
+				end
+			end
+		end
+	end
+
+	print "Answer: ", product, "\n"
+
+end
