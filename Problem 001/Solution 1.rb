5460730e4f7411fd113cf3b215aa27be5dba06e5
Shawn Smith <ShawnSmith0828@gmail.com>
2011-09-29 00:28:08 -0400

    Updated solution 1 in ruby, found a new way to make the if slightly smaller

--- Problem 001/Solution 1.rb
+++ Problem 001/Solution 1.rb
@@ -1,12 +1,11 @@
 # Solves http://projecteuler.net/problem=1
 
 if __FILE__ == $0
+
 	sum = 0
 	for counter in (1..999)
-		if ((counter % 3 == 0) || (counter % 5 == 0))
-			sum += counter
-		end
+		sum += counter if((counter%3 == 0) || (counter%5==0))
 	end
 
-	 print "Answer: ", sum, "\n"
+	print "Answer: ", sum, "\n"
 end
