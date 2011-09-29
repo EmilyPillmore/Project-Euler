3b5b0b0f80f14689b877381944b02b851d60b7ce
Shawn Smith <ShawnSmith0828@gmail.com>
2011-09-29 02:53:35 -0400

    Updated problem 6's solution in ruby slightly

--- Problem 009/Solution 9.rb
+++ Problem 009/Solution 9.rb
@@ -5,10 +5,9 @@ if __FILE__ == $0
 	for a in (1..1000)
 		for b in (1..1000)
 			for c in (1..1000)
-				if(a+b+c == 1000)
-					if(a*a + b*b == c*c)
-						product = a*b*c
-					end
+				if((a+b+c == 1000) && (a*a + b*b == c*c))
+					product = a*b*c
+					break
 				end
 			end
 		end
