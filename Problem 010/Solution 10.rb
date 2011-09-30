11eac91c8274be29505871cf7913370dfbfb0f87
Shawn Smith <ShawnSmith0828@gmail.com>
2011-09-30 12:21:12 -0400

    Updated the if statements for the ruby solution of problem 10

--- Problem 010/Solution 10.rb
+++ Problem 010/Solution 10.rb
@@ -2,19 +2,17 @@
 
 def CheckPrime(number)
 
-	if (number % 2 == 0)
-		return 0
-	end
+	return 0 if (number % 2 == 0)
+
 	x = 3
 	limit = number
 	while x < limit
-		if (number % x == 0)
-			return 0
-		end
+		return 0 if (number % x == 0)
 
 		limit = number / x
 		x += 2
 	end
+
 	return 1 
 end
 
@@ -23,9 +21,7 @@ sum = 2
 number = 3
 
 for number in (3..2000000)
-	if(CheckPrime(number) == 1)
-		sum += number
-	end
+	sum += number if (CheckPrime(number) == 1)
 end
 
 print "Answer: ", sum, "\n"
