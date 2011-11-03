a9e5d634d63d40c40029ece6e723d20971129524
Shawn Smith <ShawnSmith0828@gmail.com>
2011-11-03 15:16:30 -0400

    Now exits after the answer is found

--- Problem 009/Solution 9.rb
+++ Problem 009/Solution 9.rb
@@ -4,11 +4,9 @@ for a in (1..1000)
 	for b in (1..1000)
 		for c in (1..1000)
 			if((a+b+c == 1000) && (a*a + b*b == c*c))
-				product = a*b*c
-				break
+				print "Answer: ", a*b*c, "\n"
+				exit(0)
 			end
 		end
 	end
 end
-
-print "Answer: ", product, "\n"
