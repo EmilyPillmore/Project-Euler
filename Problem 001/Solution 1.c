f363e393e8ac96be4535e8c0651b83082a2b5e21
Shawn Smith <ShawnSmith0828@gmail.com>
2011-11-13 15:15:31 -0500

    Changed counter++ to ++counter & s = c+s to s += c

--- Problem 001/Solution 1.c
+++ Problem 001/Solution 1.c
@@ -9,11 +9,11 @@ int main()
 	int sum = 0;
 
 	/* While checking every number under 1000 */
-	for(counter = 1; counter < 1000; counter++)
+	for(counter = 1; counter <= 1000; ++counter)
 	{
 		/* If the checking number is devisible by 3 or 5
 		then add it to the sum */
-		if (counter % 3 == 0 || counter % 5 == 0) sum = counter+sum;
+		if (counter % 3 == 0 || counter % 5 == 0) sum += counter;
 	}
 
 	/* Print the answer */
