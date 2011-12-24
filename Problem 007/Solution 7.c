1aefd3876c19e20ce6d1f97ddd1ccf59c0e174b1
Shawn Smith <ShawnSmith0828@gmail.com>
2011-12-24 10:02:44 -0500

    Moved the prime checking to a header file for all C files to use.

--- Problem 007/Solution 7.c
+++ Problem 007/Solution 7.c
@@ -1,17 +1,6 @@
 /* Solves http://projecteuler.net/problem=7 */
 #include <stdio.h>
-
-/* Check if the given number is prime */
-int Prime(int number)
-{
-	/* 1 and 2 are prime, but we don't care. */
-        int x = 3;
-	for(x = 3; x < number; x += 2)
-	{
-		if(number % x == 0) return 0;
-	}
-        return 1;
-}
+#include "../includes/C/Prime.h"
 
 int main()
 {
@@ -23,7 +12,7 @@ int main()
 	while(x != 10001)
 	{
 		/* If we find a prime, incrase x, if x is 10001, break the loop */
-		if(Prime(pc) == 1) x++;
+		if(IsPrime(pc) == 1) x++;
 		pc += 2;
 	}
 
---
+++ includes/C/Prime.h
@@ -0,0 +1,33 @@
+/* Not entirely sure what these are for, but someone suggested
+	to use them, so we are. */
+#ifndef PRIME_H_24122011
+#define PRIME_H_24122011
+
+/* Declare function */
+int IsPrime(int number)
+{
+	/* 1 and 2 are both prime, so this catches them */
+	if(number < 3 && number > 0)
+		return 1;
+
+	/* Any even number other than 2 is not prime. */
+	if(number % 2 == 0)
+		return 0;
+
+	/* Declare variables */
+	int x = 3;
+	int limit = number;
+
+	/* Loop while x < limit. */
+	for(x = 3; x < limit; x += 2)
+	{
+		/* If this hits the number isn't prime. */
+		if(number % x == 0)
+			return 0;
+	}
+
+	/* Number is prime. */
+	return 1;
+}
+
+#endif
