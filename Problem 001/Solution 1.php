2e58354978d3b50fa4b489f9a5149dfd022e4cc3
Shawn Smith <ShawnSmith0828@gmail.com>
2011-12-24 11:04:03 -0500

    Added a php solution for problem 1

---
+++ Problem 001/Solution 1.php
@@ -0,0 +1,18 @@
+<?php
+/* Declare variables */
+$counter = 1;
+$sum = 0;
+
+	/* Loop while $counter < 1000 */
+	while($counter < 1000)
+	{
+		/* Check if divisible by 3 or 5 */
+		if($counter % 3 == 0 || $counter % 5 == 0) $sum += $counter;
+
+		/* Incrase counter */
+		++$counter;
+	}
+
+	/* Print answer */
+	print("Answer: " . $sum . "\r\n");
+?>
