b8b5deca1065d1997ad6fa8eccb51a93a1adcbd6
Shawn Smith <ShawnSmith0828@gmail.com>
2011-12-24 10:42:56 -0500

    Updated prime checking to use /includes/

--- Problem 010/Solution 10.rb
+++ Problem 010/Solution 10.rb
@@ -1,27 +1,11 @@
 # Solves http://projecteuler.net/problem=10
-
-def CheckPrime(number)
-
-	return 0 if (number % 2 == 0)
-
-	x = 3
-	limit = number
-	while x < limit
-		return 0 if (number % x == 0)
-
-		limit = number / x
-		x += 2
-	end
-
-	return 1 
-end
-
+require '../includes/Ruby/Prime.rb'
 
 sum = 2
 number = 3
 
 for number in (3..2000000)
-	sum += number if (CheckPrime(number) == 1)
+	sum += number if (IsPrime(number) == 1)
 end
 
 print "Answer: ", sum, "\n"
