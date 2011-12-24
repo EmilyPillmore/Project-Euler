8a3fabd1ec57578d24727ab0b3a48c0c4e41a9bb
Shawn Smith <ShawnSmith0828@gmail.com>
2011-12-24 10:25:03 -0500

    Now shows the correct answer. Heavily modified the loop.

--- Problem 007/Solution 7.c
+++ Problem 007/Solution 7.c
@@ -5,19 +5,25 @@
 int main()
 {
 	/* Set vars */
-	int x = 1;
-	int pc = 3;
+	int primecount = 0;
+	int number = 1;
 
 	/* Loop until we break */
-	while(x != 10001)
+	for(primecount = 0, number = 1; primecount != 10001; number += 2)
 	{
-		/* If we find a prime, incrase x, if x is 10001, break the loop */
-		if(IsPrime(pc) == 1) x++;
-		pc += 2;
+		printf("Count: %d\r\n", primecount);
+		/* If we find a prime, incrase x
+			if x is 10001, break the loop */
+		if(IsPrime(number) == 1)
+			primecount++;
+
+		if(primecount == 10001)
+			break;
+
 	}
 
 	/* Print the answer */
-	printf("Answer: %d\r\n",pc);
+	printf("Answer: %d\r\n", number);
 
 	return 0;
 }
