f639badeb716df5382d656bcea4a604fddeb1202
Shawn Smith <ShawnSmith0828@gmail.com>
2011-12-24 11:05:51 -0500

    Fixed spacing

--- Problem 001/Solution 1.php
+++ Problem 001/Solution 1.php
@@ -3,16 +3,16 @@
 $counter = 1;
 $sum = 0;
 
-	/* Loop while $counter < 1000 */
-	while($counter < 1000)
-	{
-		/* Check if divisible by 3 or 5 */
-		if($counter % 3 == 0 || $counter % 5 == 0) $sum += $counter;
+/* Loop while $counter < 1000 */
+while($counter < 1000)
+{
+	/* Check if divisible by 3 or 5 */
+	if($counter % 3 == 0 || $counter % 5 == 0) $sum += $counter;
 
-		/* Incrase counter */
-		++$counter;
-	}
+	/* Incrase counter */
+	++$counter;
+}
 
-	/* Print answer */
-	print("Answer: " . $sum . "\r\n");
+/* Print answer */
+print("Answer: " . $sum . "\r\n");
 ?>
