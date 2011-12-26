e9132ecad28a9ee8c5262c9943871d322ebb6b0b
Shawn Smith <ShawnSmith0828@gmail.com>
2011-12-25 19:26:50 -0500

    Removed brackets

--- Problem 003/Solution 3.c
+++ Problem 003/Solution 3.c
@@ -5,18 +5,17 @@
 int main()
 {
 	size_t number = 600851475143;
-	int i = 2;
+	int i;
 
 	/* This loops until i is the same as the number */
 	for(i = 2; i != number; i++)
-	{
 		/* If the number is divisible by i, do the math
 		otherwise continue looping */
 		if(number % i == 0)
 			number /= i;
-	}
 
 	/* Print the last number. */
 	printf("Answer: %d\r\n", number);
+
 	return 0;
 }
