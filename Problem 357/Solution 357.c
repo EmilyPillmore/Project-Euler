9b7f09789fa5ebc37a043ccfeb97f597294f19a8
Shawn Smith <ShawnSmith0828@gmail.com>
2011-12-26 11:21:34 -0500

    Used the seive function in Prime.h in Solution 357.c

--- Problem 357/Solution 357.c
+++ Problem 357/Solution 357.c
@@ -1,6 +1,7 @@
 /* Solves http://projecteuler.net/problem=357 */
 #include <stdio.h>
 #include <stdlib.h>
+#include "../includes/C/Prime.h"
 
 int main()
 {
@@ -9,19 +10,8 @@ int main()
 	int x, y, b;
 	double sum = 0;
 
-	/* Set all numbers to 1 to begin with. */
-	for(x = 1; x <= 100000000; ++x)
-		table[x] = '1';
-
-	/* Eliminate all evens except 2 */
-	for(x = 4; x <= 100000000; x += 2)
-		table[x] = '0';
-
-	/* Loop through the rest of the numbers and eliminate any number
-		that is divisible by a previous number. */
-	for(x = 3; x <= 100000000; x += 2)
-		for(y = x*2; y <= 100000000; y += x)
-			table[y] = '0';
+	/* This fills the table with the seive */
+	SeiveE(table, 100000000);
 
 	/* Loop for every number between 1 and 100m */
 	for(x = 1; x <= 100000000; ++x)
@@ -29,11 +19,11 @@ int main()
 		b = 1;
 		/* This will return 1 if d+n/d is prime.
 			(d+n/d = n+1 */
-		if(table[x+1] == '1')
+		if(table[x+1] == 1)
 		{
 			for(y = 1; y*y <= x; ++y)
 				if(x % y == 0)
-					if (table[y+x/y] == '0')
+					if (table[y+x/y] == 0)
 					{
 						b = 0;
 						break;
