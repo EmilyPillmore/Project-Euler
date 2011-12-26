cf1b7ab85a1b0a06efa00904bbdd7158e46de7ba
Shawn Smith <ShawnSmith0828@gmail.com>
2011-12-25 19:24:02 -0500

    Removed some brackets

--- Problem 001/Solution 1.c
+++ Problem 001/Solution 1.c
@@ -5,16 +5,14 @@
 int main()
 {
 	/* Set the variables. */
-	int counter = 1;
+	int counter;
 	int sum = 0;
 
 	/* While checking every number under 1000 */
 	for(counter = 1; counter < 1000; ++counter)
-	{
 		/* If the checking number is devisible by 3 or 5
 		then add it to the sum */
 		if (counter % 3 == 0 || counter % 5 == 0) sum += counter;
-	}
 
 	/* Print the answer */
 	printf("Answer: %d\r\n", sum);
