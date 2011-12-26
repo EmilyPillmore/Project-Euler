5f72b3124bc7c711181dfb0ed52143af19e840da
Shawn Smith <ShawnSmith0828@gmail.com>
2011-12-26 11:29:46 -0500

    Removed some brackets

--- Problem 010/Solution 10.c
+++ Problem 010/Solution 10.c
@@ -1,6 +1,7 @@
 /* Solves http://projecteuler.net/problem=10 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "../includes/C/Prime.h"
 
 int main()
@@ -8,14 +9,12 @@ int main()
 	/* We start off with a sum of 2 because 2 is a prime
 		and we only check odd numbers. */
 	double sum = 2;
-	int number = 3;
+	int number;
 
 	/* Loop for every odd number under 2 milion */
 	for (number = 3; number < 2000000; number += 2)
-	{
 		if(IsPrime(number))
 			sum += number;
-	}
 
 	printf("Answer: %.0f\r\n", sum);
 
