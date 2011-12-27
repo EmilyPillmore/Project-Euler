9da9dd8bccecc6636729827a66537631c5d53517
Shawn Smith <ShawnSmith0828@gmail.com>
2011-12-26 19:26:16 -0500

    Added comments to explain how it worked.

--- includes/C/Seive.h
+++ includes/C/Seive.h
@@ -4,14 +4,19 @@
 	use table[x] == '1' to see if prime, or 0 for notprime */
 void SeiveE(char table[], int number)
 {
+	/* Declare our variables */
 	int x, y;
 
+	/* Loop for every number and set them to 1 */
 	for(x = 1; x <= number; ++x)
 		table[x] = 1;
 
+	/* Loop for every even number (except 2) and set them to 0 */
 	for(x = 4; x <= number; x += 2)
 		table[x] = 0;
 
+	/* Loop for every odd number and set every number divisible by a
+		previous number to 0 */
 	for(x = 3; x <= number; x += 2)
 		for(y = x*2; y <= number; y += x)
 			table[y] = 0;
