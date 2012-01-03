0657b865ddc8edf33916a77cb0bb29dce0443458
Shawn Smith <ShawnSmith0828@gmail.com>
2012-01-03 15:54:13 -0500

    Updated an if

--- Problem 001/Solution 1.c
+++ Problem 001/Solution 1.c
@@ -12,7 +12,8 @@ int main()
 	for(counter = 1; counter < 1000; ++counter)
 		/* If the checking number is devisible by 3 or 5
 		then add it to the sum */
-		if (counter % 3 == 0 || counter % 5 == 0) sum += counter;
+		if (counter % 3 == 0 || counter % 5 == 0)
+			sum += counter;
 
 	/* Print the answer */
 	printf("Answer: %d\r\n", sum);
