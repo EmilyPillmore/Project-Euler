bed8b05ef94c6255fbd283cabb0179e96a69fb87
Shawn Smith <ShawnSmith0828@gmail.com>
2012-01-03 15:54:47 -0500

    Updated an if

--- Problem 001/Solution 1.php
+++ Problem 001/Solution 1.php
@@ -8,7 +8,8 @@ $sum = 0;
 while($counter < 1000)
 {
 	/* Check if divisible by 3 or 5 */
-	if($counter % 3 == 0 || $counter % 5 == 0) $sum += $counter;
+	if($counter % 3 == 0 || $counter % 5 == 0)
+		$sum += $counter;
 
 	/* Incrase counter */
 	++$counter;
