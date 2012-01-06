c5845d22decdd10fdfd29c2ccee42dbd47d91c02
Shawn Smith <ShawnSmith0828@gmail.com>
2012-01-06 17:49:16 -0500

    Renamed table->seive

--- includes/C/Seive.h
+++ includes/C/Seive.h
@@ -2,7 +2,7 @@
 #define SEIVEE_H_26122011
 /* This will fill seive with a table of primes between 1 and number
 	use table[x] == '1' to see if prime, or 0 for notprime */
-void SeiveE(char table[], int number)
+void SeiveE(char seive[], int number)
 {
 	/* Declare our variables */
 	int x, y;
@@ -10,16 +10,16 @@ void SeiveE(char table[], int number)
 	/* Loop for every number, set evens to 0 and odds to 1 */
 	for(x = 1; x <= number; ++x)
 		if(x % 2 == 0 && x != 2)
-			table[x] = 0;
+			seive[x] = 0;
 		else
-			table[x] = 1;
+			seive[x] = 1;
 
 	/* Loop for every odd number and set every number divisible by a
 		previous number to 0 */
 	for(x = 3; x <= number; x += 2)
-		if(table[x] == 1)
+		if(seive[x] == 1)
 			for(y = x*2; y <= number; y += x)
-				table[y] = 0;
+				seive[y] = 0;
 
 	return;
 }
