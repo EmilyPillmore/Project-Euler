63fc53ea58ea4a57441b55fe4d481d23640e7d97
Wyatt <wpillmore@gmail.com>
2012-10-27 02:28:52 -0600

    Update Problem 001/Solution 1.pl
    Always try and use perl's functional filter and map features instead of C-style for-looping if you can. 

--- Problem 001/Solution 1.pl
+++ Problem 001/Solution 1.pl
@@ -1,9 +1,5 @@
 # Solves http://projecteuler.net/problem=1
 
-$sum = 0;
-
-for $n (1..1000) {
-	$sum += $n if (($n % 3 == 0) || ($n % 5 == 0));
-}
-
-print "Answer: $sum\r\n";
+my $sum = 0;
+$sum += $_ for grep { (($_ % 3 == 0) || ($_ % 5 == 0)) } (1..1000);
+print $sum;
